87a557f3a16f8ab611d9cbb2b16afcf2e2c2a5ed
Mardarovsky <mardarovsky@gmail.com>
2021-10-11 15:25:13 +0300

    Engine improved

--- src/Engine.php
+++ src/Engine.php
@@ -7,23 +7,21 @@ use function cli\prompt;
 
 function game(string $question, callable $task): void
 {
-    $victoryCount = 0;
     $gamesCount = 3;
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    line("$question");
+    line($question);
     for ($i = 0; $i < $gamesCount; $i++) {
-        $taskText = $task();
-        line("Question: $taskText[0]");
-        $correct = $taskText[1];
-        $answer = prompt("Your answer");
-        if ($answer !== $correct) {
-            line("'$answer' is wrong answer ;(. Correct answer was '$correct'.\nLet's try again, %s!", $name);
+        [$taskText, $correctAnswer] = $task();
+        line("Question: {$taskText}");
+        $userAnswer = prompt("Your answer");
+        if ($userAnswer !== $correctAnswer) {
+            line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
+            line("Let's try again, %s!", $name);
             return;
         } else {
             line("Correct!");
-            $victoryCount++;
         }
     }
     line("Congratulations, %s!", $name);
